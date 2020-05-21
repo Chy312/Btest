6d45a2ccfd9090a47fb478369a2004518d04a87c
Chy312 <chy31212@gmail.com>
2020-05-21 15:12:32 +1000

    add new class - calculation

---
+++ src/Calculation.java
@@ -0,0 +1,7 @@
+public class Calculation {
+    public int subtraction(int a, int b){
+        int result = 0;
+        result = a-b;
+        return result;
+    }
+}
--- src/EntryHere.java
+++ src/EntryHere.java
@@ -3,5 +3,8 @@ public class EntryHere {
         String SayHi="This is the beginning (from MBP16 1:67)";
         System.out.println(SayHi);
         System.out.println("Received (From Alienware 17R4) 2:51");
+        Calculation cal= new Calculation();
+        int figure = cal.subtraction(333,21);
+        System.out.println("The calculated answer is -------"+figure);
     }
 }
